8f741125ffd880932a036a603ea7813bf601d708
Yannik Sander <me@ysndr.de>
2018-03-31 02:15:05 +0200

    initial commaand enum set

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
 fn main() {
     println!("Hello, world!");
     
+#[derive(Debug)]
+enum Command {
+    // archivar init ..
+    Init {
+        path: PathBuf,
+        with_git: bool,
+    },
+    // archivar new path ..
+    New {
+        path: PathBuf,
+        dir: PathBuf,
+
+        template: PathBuf,
+        template_attr: String,
+        template_args: Vec<String>,
+
+        no_commit: bool,
+    },
+    Archive {
+        path: PathBuf,
+        dir: PathBuf,
+        no_commit: bool,
+    },
+    Unarchive {
+        path: PathBuf,
+        dir: PathBuf,
+        no_commit: bool,
+    },
+    Empty
+}
 }
