a31a475d22e0540dc9dede939a5dca2385264ff1
Yannik Sander <me@ysndr.de>
2018-05-15 02:14:27 +0200

    add gitkeep filename

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,4 @@
 pub const ARCHIVAR_FILE_NAME: &'static str = ".archivar";
 pub const TEMPLATE_FILE_NAME: &'static str = ".template.yaml";
 pub const PROJECT_FILE_NAME: &'static str = ".archivarproject";
+pub const GITKEEP_FILE_NAME: &'static str = ".gitkeep";
