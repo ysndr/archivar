d6df5e27929dc6210f15ae1eaeca5af8a814c8a9
Yannik Sander <me@ysndr.de>
2018-08-08 20:40:15 +0200

    impl ActionTrait for OS::Action

--- src/action/os.rs
+++ src/action/os.rs
@@ -1,18 +1,125 @@
+use action::ActionTrait;
+use app;
+use error::*;
+use std::fs;
 use std::path::PathBuf;
+use std::process::Command;
 
+/// OS Specific Actions
+///
+/// Basically, the enum variants hold information about target paths used to
+/// perform filesystem related tasks in the ActionTrait implementation
 #[derive(Debug, PartialEq)]
 pub enum Action {
-    Mkdir { path: PathBuf },
-    Touch { path: PathBuf, mkparents: bool },
-    Move { from: PathBuf, to: PathBuf },
-    Copy { from: PathBuf, to: PathBuf },
-    Chmod { target: PathBuf, mode: u32 },
+    Mkdir {
+        path: PathBuf,
+    },
 
-    Shell(String, PathBuf),
+    Touch {
+        path: PathBuf,
+        mkparents: bool,
+    },
+
+    Move {
+        from: PathBuf,
+        to: PathBuf,
+    },
+
+    Copy {
+        from: PathBuf,
+        to: PathBuf,
+    },
+
+    Chmod {
+        target: PathBuf,
+        ro: bool,
+    },
+
+    Shell {
+        command: String,
+        cwd: Option<PathBuf>,
+    },
 }
 
 impl From<Action> for super::Action {
     fn from(action: Action) -> super::Action {
         super::Action::OS(action)
+    }
+}
+
+/// ActionTraitt implementation
+///
+/// Mostly proxy the `std` provided methods.
+/// For shell commands we reach out for a bash shell
+/// to run the given command sring in
+impl ActionTrait for Action {
+    fn run<'a>(&self, context: &'a app::Context) -> Result<()> {
+        use action::os::Action::*;
+
+        let root = &context.path;
+        let mut shell = context.shell();
+
+        match self {
+            // create folders
+            Mkdir { path } => {
+                info!("mkdir ({})", path.display());
+                fs::create_dir_all(root.join(path))?;
+            }
+
+            // create files
+            Touch { path, mkparents } => {
+                info!("touch ({})", path.display());
+                fs::File::create(root.join(path))?;
+            }
+            
+            // move files/folders
+            Move { from, to } => {
+                info!("move ({} -> {})", from.display(), to.display());
+                fs::rename(root.join(from), root.join(to))?;
+            }
+
+            // copy files/folders
+            Copy { from, to } => {
+                info!("copy ({} -> {})", from.display(), to.display());
+                fs::copy(root.join(from), root.join(to))?;
+            }
+
+            // chmod files/folders
+            Chmod { target, ro } => {
+                info!(
+                    "chmod ({} := {})",
+                    target.display(),
+                    if *ro { "ro" } else { "rw" }
+                );
+
+                let mut perms = fs::metadata(root.join(target))?.permissions();
+                perms.set_readonly(*ro);
+                fs::set_permissions(root.join(target), perms)?;
+            }
+
+            // run shell commands
+            Shell { command, cwd } => {
+                                    
+                let mut base = Command::new("sh");
+
+                let process = base.arg("-c").arg(command);
+
+                let process = if let Some(cwd) = cwd {
+                    process.current_dir(cwd)
+                } else { process };
+
+
+                shell.info(format!("command: `{}`", command)).unwrap();
+
+                let status = process.status()?;
+
+                shell.info(match status.code() {
+                    Some(code) => format!("command exited with status code: {}", code),
+                    None => "command terminated by signal".to_string(),
+                }).unwrap();
+            }
+        };
+
+        Ok(())
     }
 }
